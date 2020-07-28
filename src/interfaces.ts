92376db696d88d4cbd49c312e7975ea900014c74
Barry Michael Doyle <barry.doyle@universal.one>
2020-07-28 16:41:10 +0200

    setup for deno

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,60 +1,60 @@
 export interface Config {
-  proxyUrl?: string
-  clientId: string
-  secret: string
+  proxyUrl?: string;
+  clientId: string;
+  secret: string;
 }
 
 export interface GetAccountsResponse {
   data: {
     accounts: {
-      accountId: string
-      accountNumber: string
-      accountName: string
-      referenceName: string
-      productName: string
-    }[]
-  }
+      accountId: string;
+      accountNumber: string;
+      accountName: string;
+      referenceName: string;
+      productName: string;
+    }[];
+  };
   links: {
-    self: string
-  }
+    self: string;
+  };
   meta: {
-    totalPages: number
-  }
+    totalPages: number;
+  };
 }
 
 export interface GetAccountTransactionResponse {
   data: {
     transactions: {
-      accountId: string
-      type: string
-      status: string
-      description: string
-      cardNumber: string
-      postingData: string
-      valueDate: string
-      actionDate: string
-      amount: number
-    }[]
-  }
+      accountId: string;
+      type: string;
+      status: string;
+      description: string;
+      cardNumber: string;
+      postingData: string;
+      valueDate: string;
+      actionDate: string;
+      amount: number;
+    }[];
+  };
   links: {
-    self: string
-  }
+    self: string;
+  };
   meta: {
-    totalPages: number
-  }
+    totalPages: number;
+  };
 }
 
 export interface GetAccountBalanceResponse {
   data: {
-    accountId: string
-    currentBalance: number
-    availableBalance: number
-    currency: string
-  }
+    accountId: string;
+    currentBalance: number;
+    availableBalance: number;
+    currency: string;
+  };
   links: {
-    self: string
-  }
+    self: string;
+  };
   meta: {
-    totalPages: number
-  }
+    totalPages: number;
+  };
 }
